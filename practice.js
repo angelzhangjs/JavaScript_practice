934bd407255f77ae0fb9bc2d232556677f761099
angelzhangjs <az.coding.2021@gmail.com>
2023-06-13 12:35:39 -0700

    add_class

---
+++ practice.js
@@ -0,0 +1,11 @@
+class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname; 
+        this.lastname = lastname;
+    }
+}
+
+// test the person class by creating an object 
+const newPerson = new Person('angel', 'zhang'); 
+console.log(newPerson.firstname)
+
